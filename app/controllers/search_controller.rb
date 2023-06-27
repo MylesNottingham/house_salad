5c9f3594629fec53bf5e63963b601c6f598e83b7
Myles Nottingham <MylesNottingham@gmail.com>
2023-06-27 15:27:47 -0600

    Feat: Remove hardcoded key from code

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,7 @@ class SearchController < ApplicationController
     state = params[:state]
 
     conn = Faraday.new(url: "https://api.propublica.org") do |faraday|
-      faraday.headers["X-API-Key"] = "S9JON3ruNOI6XiyymcnZ7gtsjnToPxuXyT0bgeaX"
+      faraday.headers["X-API-Key"] = ENV["PROPUBLICA_API_KEY"]
     end
 
     response = conn.get("congress/v1/members/house/#{state}/current.json")
